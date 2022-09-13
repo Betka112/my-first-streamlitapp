c084e79d72a8ce54aaf2d659a8085c871da7b07d
Betka112 <alzbeta.bohinikova@gmail.com>
2022-09-13 11:04:36 +0200

    add all files

---
+++ src/app.py
@@ -0,0 +1,133 @@
+# Streamlit live coding script
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from urllib.request import urlopen
+import json
+from copy import deepcopy
+
+
+# First some MPG Data Exploration
+@st.cache
+# decorator = what does it do?
+def load_data(path):
+    df = pd.read_csv(path)
+    return df
+# df is accesible in cache
+
+
+mpg_df_raw = load_data(path="./data/mpg.csv")
+# raw stays untouched
+mpg_df = deepcopy(mpg_df_raw)
+# cache looks for changes
+
+# Add title and header
+st.title("Introduction to Streamlit")
+st.header("MPG Data Exploration")
+
+# it's possible to create a sidebar
+
+# Widgets: checkbox (you can replace st.xx with st.sidebar.xx)
+
+# you could store the info about the box in a variable
+# and this creates the inter activity
+if st.checkbox("Show Dataframe"):
+    # it's a boolean when it's chceked it's True
+    st.subheader("This is my dataset:")
+    st.dataframe(data=mpg_df)
+    # st.table(data=mpg_df)
+
+# Setting up columns
+# left_column, right_column = st.columns(2)
+# len dva stlpce
+left_column, middle_column, right_column = st.columns([3, 1, 1])
+# vektor mi hovori ze ten lavy ma mat sirku 3 a tie zvysne dva iba 1
+
+# Widgets: selectbox
+years = ["All"]+sorted(pd.unique(mpg_df['year']))
+year = left_column.selectbox("Choose a Year", years)
+# vyraba vyrolovacie okno
+
+# Widgets: radio buttons
+show_means = middle_column.radio(
+    label='Show Class Means', options=['Yes', 'No'])
+
+# tu si vyberiem, ktory typ grafu chcem mat
+plot_types = ["Matplotlib", "Plotly"]
+plot_type = right_column.radio("Choose Plot Type", plot_types)
+
+# Flow control and plotting
+if year == "All":
+    reduced_df = mpg_df
+else:
+    reduced_df = mpg_df[mpg_df["year"] == year]
+
+means = reduced_df.groupby('class').mean()
+
+# In Matplotlib
+m_fig, ax = plt.subplots(figsize=(10, 8))
+ax.scatter(reduced_df['displ'], reduced_df['hwy'], alpha=0.7)
+
+if show_means == "Yes":
+    ax.scatter(means['displ'], means['hwy'], alpha=0.7, color="red")
+
+ax.set_title("Engine Size vs. Highway Fuel Mileage")
+ax.set_xlabel('Displacement (Liters)')
+ax.set_ylabel('MPG')
+
+# In Plotly
+p_fig = px.scatter(reduced_df, x='displ', y='hwy', opacity=0.5,
+                   range_x=[1, 8], range_y=[10, 50],
+                   width=750, height=600,
+                   labels={"displ": "Displacement (Liters)",
+                           "hwy": "MPG"},
+                   title="Engine Size vs. Highway Fuel Mileage")
+p_fig.update_layout(title_font_size=22)
+
+if show_means == "Yes":
+    p_fig.add_trace(go.Scatter(x=means['displ'], y=means['hwy'],
+                               mode="markers"))
+    p_fig.update_layout(showlegend=False)
+
+# Select which plot to show
+if plot_type == "Matplotlib":
+    st.pyplot(m_fig)
+else:
+    st.plotly_chart(p_fig)
+
+# We can write stuff
+url = "https://archive.ics.uci.edu/ml/datasets/auto+mpg"
+st.write("Data Source:", url)
+# "This works too:", url
+
+# Another header
+st.header("Maps")
+
+# Sample Streamlit Map
+st.subheader("Streamlit Map")
+# automaticky rozlisuje ci je v mape langitude a lattude
+# datasets are already included in plotly, and other libraries
+ds_geo = px.data.carshare()
+ds_geo['lat'] = ds_geo['centroid_lat']
+ds_geo['lon'] = ds_geo['centroid_lon']
+st.map(ds_geo)
+
+# Sample Choropleth mapbox using Plotly GO
+st.subheader("Plotly Map")
+
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
+                 dtype={"fips": str})
+
+plotly_map = go.Figure(go.Choroplethmapbox(geojson=counties, locations=df.fips, z=df.unemp,
+                                    colorscale="Viridis", zmin=0, zmax=12,
+                                    marker_opacity=0.5, marker_line_width=0))
+plotly_map.update_layout(mapbox_style="carto-positron",
+                  mapbox_zoom=3, mapbox_center={"lat": 37.0902, "lon": -95.7129},
+                  margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+# toto prida plotly chart
+st.plotly_chart(plotly_map)
